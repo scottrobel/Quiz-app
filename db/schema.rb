9ab7e0b4ef94e94ecbccf9d3304974c190029d00
scottrobel <robelrain@gmail.com>
2020-02-22 18:17:52 -0500

    create question answers join model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_224158) do
+ActiveRecord::Schema.define(version: 2020_02_22_224810) do
 
   create_table "answers", force: :cascade do |t|
     t.string "contents"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "question_answers", force: :cascade do |t|
+    t.integer "question_id", null: false
+    t.integer "answer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["answer_id"], name: "index_question_answers_on_answer_id"
+    t.index ["question_id"], name: "index_question_answers_on_question_id"
   end
 
   create_table "question_choices", force: :cascade do |t|
@@ -74,6 +83,8 @@ ActiveRecord::Schema.define(version: 2020_02_22_224158) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "question_answers", "answers"
+  add_foreign_key "question_answers", "questions"
   add_foreign_key "question_choices", "answers"
   add_foreign_key "question_choices", "questions"
   add_foreign_key "questions", "quizzes"

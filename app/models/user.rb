ffdf36a6f0c1f881e70807618ffd5e6cdc0cc93d
scottrobel <robelrain@gmail.com>
2020-02-22 15:17:00 -0500

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  enum user_type: [:default_user, :admin]
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :rememberable, :validatable
+end

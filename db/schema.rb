b2ccf474a3d86c0db3b0cf8e9ed9d4e0c2fe1186
scottrobel <robelrain@gmail.com>
2020-02-22 22:01:42 -0500

    remove index that was breaking user model

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 2020_02_22_224810) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

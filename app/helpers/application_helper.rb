259704ea0db7e89dda018e6866c1006e278d4659
scottrobel <robelrain@gmail.com>
2020-02-28 01:47:04 -0500

    style nav_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  private
+
+  def navbar_link_class(link)
+    if request.path == link
+      'active'
+    else
+      ''
+    end
+  end
+
+  def navbar_link(text, link, html_params={})
+    html_params[:class] = navbar_link_class(link)
+    link_to text, link, html_params
+  end
 end

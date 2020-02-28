9faf86ea6035aea43efbdcafed8076f37e761e2a
scottrobel <robelrain@gmail.com>
2020-02-27 21:18:25 -0500

    fix bug in 'require_admin_or_own_profile' helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   private
 
   def require_admin_or_own_profile
-    unless current_user.admin_user? || current_user.id == params[:id]
+    unless current_user.admin_user? || current_user.id == params[:id].to_i
       flash[:alert] = 'you must be an admin to view other profiles!'
       redirect_to root_path
     end

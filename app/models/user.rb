5b58d92f4ace308d77e4fd8e20b49aa5162cbbaa
Scott Robel Jr <scott.robel.jr@gmail.com>
2020-08-11 15:54:26 -0400

    add user_index json TEMPORARY

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 class User < ApplicationRecord
   enum user_type: %i[regular_user admin_user]
-  has_many :quizzes, foreign_key: 'creator_id'
-  has_many :responses
+  has_many :quizzes, foreign_key: 'creator_id', dependent: :destroy
+  has_many :responses, dependent: :destroy
   has_many :taken_quizzes, through: :responses, class_name: 'Quiz', source: :quiz
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   end
   resources :responses, only: %i[create show]
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

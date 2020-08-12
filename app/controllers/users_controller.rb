196bf5680f7f5feb2681598e30725cd5395bd301
Scott Robel Jr <scott.robel.jr@gmail.com>
2020-08-12 12:52:32 -0400

    hide users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   include UsersHelper
+  before_action :require_admin_or_own_profile, only: [:show]
   def show
     @user = User.find_by(id: params[:id])
   end

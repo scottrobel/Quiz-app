2122f30fef3dea1246b3ec2f4c435a83f37bad6d
Scott Robel Jr <scott.robel.jr@gmail.com>
2020-08-17 03:41:50 -0400

    fix bugs in admin filters

--- app/helpers/compass_photos_helper.rb
+++ app/helpers/compass_photos_helper.rb
@@ -11,7 +11,7 @@ module CompassPhotosHelper
     end
   end
   def require_own_quiz_or_admin
-    unless current_user.quizzes.find_by(id: params[:id]) || current_user.admin_user?
+    unless current_user.quizzes.find_by(id: params[:quiz_id] || params[:id]) || current_user.admin_user?
       flash[:alert] = 'That Quiz is not Yours'
       redirect_to root_path
     end

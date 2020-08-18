01031a4375da95b5ae59e933c65a954ea66387a3
Scott Robel Jr <scott.robel.jr@gmail.com>
2020-08-18 15:00:39 -0400

    add storage to deploy.rb symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,8 @@ set :repo_url, "git@github.com:scottrobel/Quiz-app.git"
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs,'storage' , 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs,'storage', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5

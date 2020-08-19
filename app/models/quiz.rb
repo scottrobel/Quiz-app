7c42f5b1e15adb5ef72db2ffe52582d9b0c9fc60
Scott Robel Jr <scott.robel.jr@gmail.com>
2020-08-19 16:04:27 -0400

    simplify xy_min and xy_max methods

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -11,38 +11,24 @@ class Quiz < ApplicationRecord
   has_many :responses, dependent: :destroy
   accepts_nested_attributes_for :questions, allow_destroy: true
   #== Instance Methods
-
-  def axis_question_hash
-    self.questions.includes(:answers).group_by(&:axis)
-  end
-
+  
   def xy_max
-    axis_max_values = axis_question_hash.map do |axis, questions|
-      max_value = questions.map do |question|
-        question.answers.map(&:value).max
-      end.compact.sum
-      [axis, max_value]
-    end.to_h
-    {'X' => axis_max_values['X'], 'Y' => axis_max_values['Y']}
+    {'X' => axis_min_max("X", "max"), 'Y' => axis_min_max("Y", "max")}
   end
 
   def xy_min
-    axis_max_values = axis_question_hash.map do |axis, questions|
-      max_value = questions.map do |question|
-        question.answers.map(&:value).min
-      end.compact.sum
-      [axis, max_value]
-    end.to_h
-    {'X' => axis_max_values['X'], 'Y' => axis_max_values['Y']}
+    {'X' => axis_min_max("X", "min"), 'Y' => axis_min_max("Y", "min")}
   end
 
   def update_indexes
-    self.questions.to_a.sort_by(&:index).each_with_index do |question, question_index|
-      question.index = question_index
-      question.save
-      question.answers.order(:index).each_with_index do |answer, answer_index|
-        answer.index = answer_index
-        answer.save
+    Task.transaction do
+      self.questions.to_a.sort_by(&:index).each_with_index do |question, question_index|
+        question.index = question_index
+        question.save
+        question.answers.order(:index).each_with_index do |answer, answer_index|
+          answer.index = answer_index
+          answer.save
+        end
       end
     end
   end
@@ -54,5 +40,16 @@ class Quiz < ApplicationRecord
         answer.index = answer_index
       end
     end
+  end
+
+  private
+
+  def axis_min_max(axis, min_or_max)
+    min_or_max = min_or_max == "min" ? "min" : "max"
+    self.questions
+      .joins("INNER JOIN answers ON answers.question_id = questions.id")
+      .having("questions.axis = ?", axis)
+      .group('questions.id')
+      .pluck("#{min_or_max}(answers.value)").sum
   end
 end

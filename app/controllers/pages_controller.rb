6a2cea39ebc277ade3346b75dcb8970103d14b53
Scott Robel Jr <scott.robel.jr@gmail.com>
2020-08-30 03:19:11 -0400

    make front page features

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@
 
 class PagesController < ApplicationController
   def home
-    @quizzes = Quiz.all
+    @quizzes = Quiz.all.joins("INNER JOIN features ON features.quiz_id = quizzes.id").order("features.created_at DESC")
   end
 end
